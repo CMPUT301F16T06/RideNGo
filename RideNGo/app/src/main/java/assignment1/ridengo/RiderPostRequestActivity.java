f82e5a68f147edddbcd9f00b48a83dfa0f0c2419
Simpleple <zhao2@ualberta.ca>
2016-10-23 16:22:27 -0600

    add activities to uml

---
+++ RideNGo/app/src/main/java/assignment1/ridengo/RiderPostRequestActivity.java
@@ -0,0 +1,13 @@
+package assignment1.ridengo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RiderPostRequestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rider_post_request);
+    }
+}
--- RideNGo/app/src/main/java/assignment1/ridengo/RoleSelectActivity.java
+++ RideNGo/app/src/main/java/assignment1/ridengo/RoleSelectActivity.java
@@ -3,11 +3,11 @@ package assignment1.ridengo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class RoleSelectActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_role_select);
     }
 }
