402848a92e421d28ae2d865a75e83836c61f0a2f
unknown <>
2016-11-08 20:24:43 -0700

    Driver's main layout done

--- RideNGo/app/src/main/java/assignment1/ridengo/DriverMainActivity.java
+++ RideNGo/app/src/main/java/assignment1/ridengo/DriverMainActivity.java
@@ -9,5 +9,8 @@ public class DriverMainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_main);
+
+
     }
+
 }
