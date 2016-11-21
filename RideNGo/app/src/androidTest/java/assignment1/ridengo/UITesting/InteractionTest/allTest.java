93df325c2bd0ff458f75072b6c1cb7d05fbd909a
rwu780 <rwu4@ualberta.ca>
2016-11-20 21:20:21 -0700

    Add interaction test, 3 fail

---
+++ RideNGo/app/src/androidTest/java/assignment1/ridengo/UITesting/InteractionTest/allTest.java
@@ -0,0 +1,27 @@
+package assignment1.ridengo.UITesting.InteractionTest;
+
+import android.app.Activity;
+import android.sax.TextElementListener;
+import android.test.ActivityInstrumentationTestCase2;
+
+import junit.framework.TestSuite;
+
+/**
+ * Created by Rui on 2016-11-20.
+ */
+public class allTest extends ActivityInstrumentationTestCase2<Activity> {
+    public allTest(){
+        super(Activity.class);
+    }
+
+    public static TestSuite suite(){
+        TestSuite t = new TestSuite();
+        t.addTestSuite(addRider.class);
+        t.addTestSuite(addDriver.class);
+        t.addTestSuite(postRequest.class);
+        t.addTestSuite(acceptRequest.class);
+        t.addTestSuite(confirmDriver.class);
+        t.addTestSuite(CompleteTrip.class);
+        return t;
+    }
+}
